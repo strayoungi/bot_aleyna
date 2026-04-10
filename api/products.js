2351c45c849fef4abcd11c4e342196e1a7d838b2
Aleyna <starnivty@example.com>
2026-04-11 02:05:49 +0700

    nambah api get product, testing dulu

---
+++ api/products.js
@@ -0,0 +1,20 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const { data, error } = await supabase
+      .from("products")
+      .select("*");
+
+    if (error) return res.status(500).json({ error });
+
+    return res.status(200).json({ data });
+  }
+
+  return res.status(405).end();
+}
